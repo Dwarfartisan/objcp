774d96b67046f8a4337ef37b8b419cd74157c6b6
jnriver <jnriver1986@gmail.com>
2012-11-09 15:56:56 +0800

    28.1 修改
    重新建立了项目

--- jnriver/source/28.1/TahDoodle/TahDoodle/BNRDocument.m
+++ jnriver/source/28.1/TahDoodle/TahDoodle/BNRDocument.m
@@ -2,7 +2,7 @@
 //  BNRDocument.m
 //  TahDoodle
 //
-//  Created by nangua on 12-11-7.
+//  Created by nangua on 12-11-9.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
@@ -10,50 +10,72 @@
 
 @implementation BNRDocument
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        // Add your subclass-specific initialization here.
-    }
-    return self;
-}
-
+#pragma NSDocument的覆盖方法
 - (NSString *)windowNibName
 {
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
     return @"BNRDocument";
 }
 
-- (void)windowControllerDidLoadNib:(NSWindowController *)aController
+#pragma 动作方法
+- (IBAction)createNewItem:(id)sender
 {
-    [super windowControllerDidLoadNib:aController];
-    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+    if(!todoItems){
+        todoItems = [NSMutableArray array];
+    }
+    [todoItems addObject:@"New Item"];
+    
+//    NSLog(@"todoItems:\n%@",todoItems);
+    // reloadData 刷新
+    [itemTableView reloadData];
+    
+    [self updateChangeCount:NSChangeDone];
 }
 
-+ (BOOL)autosavesInPlace
+#pragma 数据源方法
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
 {
-    return YES;
+    return [todoItems count];
+}
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn 
+            row:(NSInteger)row
+{
+    return [todoItems objectAtIndex:row];
+}
+- (void)tableView:(NSTableView *)tableView 
+   setObjectValue:(id)object 
+   forTableColumn:(NSTableColumn *)
+  tableColumn row:(NSInteger)row
+{
+    [todoItems replaceObjectAtIndex:row withObject:object]; 
+    
+    [self updateChangeCount:NSChangeDone];
 }
 
+/*
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return nil;
+    if(!todoItems){
+        todoItems = [NSMutableArray array];
+    }
+    
+    // 将todoItems 转化为NSData对象
+    NSData *data = [NSPropertyListSerialization 
+                    dataWithPropertyList:todoItems
+                                  format:NSPropertyListBinaryFormat_v1_0 
+                                 options:0 error:outError];
+    return data;
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return YES;
+    // 将NSData对象 转化回todoItems
+    todoItems = [NSPropertyListSerialization
+                 propertyListWithData:data
+                              options:NSPropertyListBinaryFormat_v1_0
+                               format:NULL 
+                                error:outError];
+    
+    return (todoItems != nil);
 }
-
+*/
 @end
